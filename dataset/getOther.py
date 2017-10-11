a49030f359105c819085c95158f9acd66527efc5
Jack <furbyjl@cardiff.ac.uk>
2017-10-11 10:38:07 +0100

    getOther now gets URL's of images of not receipt

--- dataset/getOther.py
+++ dataset/getOther.py
@@ -5,19 +5,24 @@ import csv
 # array to store all image URL's
 allImages = []
 
-# gets URL of each image on https://www.pexels.com
-page = requests.get('https://www.pexels.com/search/paper/' )
-tree = html.fromstring(page.content)
-allImages +=  tree.xpath('//img[@class="photo-item__img"]/@src')
+# gets URL of each image on each page on www.image-net.org
+# n06263609 = printed media, n14974264 = paper, n06793231 = sign, n04152593 = screen
+searchTerms = ['n06263609', 'n14974264', 'n06793231', 'n04152593']
 
-# writes CSV file with image URL in format image URL, image name, 1
+for pageSearch in range(len(searchTerms)):
+    page = requests.get('http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=%s' % searchTerms[pageSearch])
+    tree = html.fromstring(page.content)
+    images = tree.xpath('//html//body//*/text()')
+    allImages += images[0].splitlines()
+
+# writes CSV file with image URL in format image URL, image name, 0
 # image URL = URL of the image
 # image name = name of image once downloaded (will rename image in seperate script)
-# 0 = image not of a receipt
-csvfile = open('other.csv', 'w', newline='')
+# 0 = image is not of a receipt
+csvfile = open('notReceipt.csv', 'w', newline='', encoding='utf-8')
 writer = csv.writer(csvfile, delimiter=",")
 imageNum = 1
 for row in allImages:
-    writer.writerow([row, "other-" + str(imageNum), "0"])
+    writer.writerow([row, "notReceipt-" + str(imageNum), "0"])
     imageNum += 1
 csvfile.close()
