2e618bca92b871ea34f865e97af0810e2dc11280
Jack <furbyjl@cardiff.ac.uk>
2017-11-26 11:28:53 +0000

    added softmax layer + cross-entropy loss, updated creation of weights to be between 0 and 1 if not already set

--- cnn/train.py
+++ cnn/train.py
@@ -17,7 +17,7 @@ def getData(inputFile):
 def convolution(image, filterSize, stride, padding, filterLayers, filters=None, bias=None):
 	if (((image.shape[0] - filterSize[0] + 2 * padding) / stride) + 1).is_integer():  # checks output is an int
 		if (((image.shape[1] - filterSize[1] + 2 * padding) / stride) + 1).is_integer():  # checks output is an int
-			outshape = np.zeros([int((((image.shape[0] - filterSize[0] + 2 * padding) / stride) + 1)), int((((image.shape[1] - filterSize[1] + 2 * padding) / stride) + 1)), filterLayers], dtype=int)  # creats the activation map (empty)
+			outshape = np.zeros([int((((image.shape[0] - filterSize[0] + 2 * padding) / stride) + 1)), int((((image.shape[1] - filterSize[1] + 2 * padding) / stride) + 1)), filterLayers], dtype=np.float64)  # creats the activation map (empty)
 			image = np.pad(image, ((padding,padding), (padding,padding), (0,0)), mode='constant')  # adds padding of 0's arround image
 		else:
 			print("Convolution is not possible. Try a different stride or padding")
@@ -30,15 +30,13 @@ def convolution(image, filterSize, stride, padding, filterLayers, filters=None, 
 	if filters == None:
 		filters = []
 		for i in range(filterLayers):
-			filters.append(np.zeros([filterSize[0], filterSize[1], image.shape[2]], dtype=int))
+			filters.append(np.random.rand(filterSize[0], filterSize[1], image.shape[2]))
 
 	# Creats bias if they dont already exist
 	if bias == None:
 		bias = []
 		for i in range(filterLayers):
-			newBias = np.zeros([1, 1, 1], dtype=int)
-			newBias[0, 0, 0] = 0
-			bias.append(newBias)
+			bias.append(np.random.rand(1, 1, 1))
 
 	# Goes over each section of the input image by size specified by the filter and sums up element wise matrix multiplication to the output
 	for i in range(outshape.shape[2]):  # number of filters
@@ -48,16 +46,17 @@ def convolution(image, filterSize, stride, padding, filterLayers, filters=None, 
 				outshape[k, j, i] = np.sum(image[k*stride:filterArray.shape[0] + k*stride, j*stride:filterArray.shape[1] + j*stride, :] * filterArray[:, :, :]) + bias[i]
 	return outshape, filters, bias  # result, filters and bias returned
 
+
 # performs max pooling on a input numpy array. Output will just be a numpy array with max pooling applied
 def maxPooling(image, filterSize, stride):
 	if (((image.shape[0] - filterSize[0]) / stride) + 1).is_integer():  # checks output is an int
 		if (((image.shape[1] - filterSize[1]) / stride) + 1).is_integer():  # checks output is an int
-			outshape = np.zeros([int(((image.shape[0] - filterSize[0]) / stride + 1)), int(((image.shape[1] - filterSize[1]) / stride + 1)), image.shape[2]], dtype=int)  # creats the output array
+			outshape = np.ones([int(((image.shape[0] - filterSize[0]) / stride + 1)), int(((image.shape[1] - filterSize[1]) / stride + 1)), image.shape[2]], dtype=np.float64)  # creats the output array
 		else:
-			print("Convolution is not possible. Try a different stride or filter size")
+			print("Pooling is not possible. Try a different stride or filter size")
 			return
 	else:
-		print("Convolution is not possible. Try a different stride or filter size")
+		print("Pooling is not possible. Try a different stride or filter size")
 		return
 
 	for i in range(outshape.shape[2]):  # number of layers
@@ -66,9 +65,21 @@ def maxPooling(image, filterSize, stride):
 				outshape[k, j, i] = np.amax(image[k*stride:k*stride + filterSize[0], j*stride:j*stride + filterSize[1], :])
 	return outshape
 
+
 # performs ReLu on a given numpy array
 def relu(image):
 	return image.clip(min=0)
+
+# takes in inputArray and expected result, returns predictions and error
+def softMax(inputArray, expectedResult):
+	# http://cs231n.github.io/linear-classify/
+	inputArray.astype(np.float64)
+	inputArray -= np.max(inputArray)
+	inputArray = np.tanh(inputArray)  # look into replacing this in the future?
+	output = np.exp(inputArray) / np.sum(np.exp(inputArray))
+	expectedResult = np.array(expectedResult)
+	error = -np.sum(np.multiply(expectedResult, np.log(output)) + np.multiply((1-expectedResult), np.log(1-output)))
+	return output, error
 
 # This is test data - delete this once finnished makeing CNN
 def exampleTestData():
@@ -90,7 +101,7 @@ def exampleTestData():
 						[0, 1, 1, 1, 0],
 						[2, 2, 1, 2, 1]])
 
-	testArray = np.zeros([5, 5, 3], dtype=int)
+	testArray = np.zeros([5, 5, 3], dtype=np.float64)
 
 	testArray[:, :, 0] = test1
 	testArray[:, :, 1] = test2
@@ -108,7 +119,7 @@ def exampleTestData():
 						[-1, 0, 1],
 						[1, 1, 1]])
 
-	w0 = np.zeros([3, 3, 3], dtype=int)
+	w0 = np.zeros([3, 3, 3], dtype=np.float64)
 
 	w0[:, :, 0] = w0Test1
 	w0[:, :, 1] = w0Test2
@@ -126,7 +137,7 @@ def exampleTestData():
 						[1, -1, 0],
 						[0, -1, 0]])
 
-	w1 = np.zeros([3, 3, 3], dtype=int)
+	w1 = np.zeros([3, 3, 3], dtype=np.float64)
 
 	w1[:, :, 0] = w1Test1
 	w1[:, :, 1] = w1Test2
@@ -134,19 +145,85 @@ def exampleTestData():
 
 	testFilters = [w0, w1]
 
-	b1 = np.zeros([1, 1, 1], dtype=int)
+	b1 = np.zeros([1, 1, 1], dtype=np.float64)
 	b1[0, 0, 0] = 1
-	b2 = np.zeros([1, 1, 1], dtype=int)
+	b2 = np.zeros([1, 1, 1], dtype=np.float64)
 	b2[0, 0, 0] = 0
 
 	biasTest = [b1, b2]
 
+	Flatw1 = np.array([[-1, 1, 1],
+						[0, 1, -1],
+						[1, 1, 1]])
+
+	Flatw2 = np.array([[0, 1, -1],
+						[1, 0, 1],
+						[0, -1, 1]])
+
+	Flatw3 = np.array([[1, 0, 1],
+						[1, 0, -1],
+						[1, 0, 0]])
+
+	Flatw4 = np.array([[1, 1, 1],
+						[0, 1, -1],
+						[0, -1, 1]])
+
+	Flatw5 = np.array([[0, 0, 0],
+						[1, 1, 1],
+						[-1, -1, -1]])
+
+	Flatw6 = np.array([[0, 0, 1],
+						[1, -1, 1],
+						[1, 1, 0]])
+
+	Flatw7 = np.array([[0, 0, -1],
+						[1, 1, 1],
+						[1, 0, -1]])
+
+	Flatw8 = np.array([[0, -1, 0],
+						[-1, 0, -1],
+						[0, -1, 0]])
+
+	Flatw9 = np.array([[-1, -1, -1],
+						[1, -1, -1],
+						[1, 1, 1]])
+
+	Flatw10 = np.array([[0, 0, 0],
+						[1, 1, 1],
+						[0, 0, 0]])
+
+	flatFilterTest = np.zeros([3, 3, 10], dtype=np.float64)
+
+	flatFilterTest[:, :, 0] = Flatw1
+	flatFilterTest[:, :, 1] = Flatw2
+	flatFilterTest[:, :, 2] = Flatw3
+	flatFilterTest[:, :, 3] = Flatw4
+	flatFilterTest[:, :, 4] = Flatw5
+	flatFilterTest[:, :, 5] = Flatw6
+	flatFilterTest[:, :, 6] = Flatw7
+	flatFilterTest[:, :, 7] = Flatw8
+	flatFilterTest[:, :, 8] = Flatw9
+	flatFilterTest[:, :, 9] = Flatw10
+
+	testFlatFilters = [flatFilterTest]
+
 	outputData, conFilters, conBias = convolution(testArray, (3, 3), 2, 1, 2, testFilters, biasTest)
 	# print(outputData)
+	# print(outputData.shape)
+	outputDataPooling = maxPooling(outputData, (2, 2), 1)
+	# print(outputDataPooling)
+	outputRelu = relu(outputData)
+	# print(outputRelu)
+	# print(outputRelu.shape)
+	outputFlatten, flattenFilters, flattenBias = convolution(outputRelu, (outputRelu.shape[0], outputRelu.shape[1]), 1, 0, 10) # flatten
+	outputFlatten2, flattenFilters2, flattenBias2 = convolution(outputRelu, (outputRelu.shape[0], outputRelu.shape[1]), 1, 0, 2) # flatten
+	# outputFC, weightsFC, biasFC = fullyConnected(outputFlatten, 2)
+	# print(outputFC)
+	softmaxResult, softmaxError = softMax(outputFlatten2[0][0], [0, 1])
 
 	# testing for pooling
 
-	testPooling = np.empty([4, 4, 1], dtype=int)
+	testPooling = np.empty([4, 4, 1], dtype=np.float64)
 
 	testPooling[:, :, 0] = np.array([[1, 1, 2, 4],
 							[5, 6, 7, 8],
@@ -163,6 +240,15 @@ def exampleTestData():
 	# print(outputRelu.shape)
 	# print(outputRelu)
 
+	outputFlatten, flattenFilters, flattenBias = convolution(outputRelu, (outputRelu.shape[0], outputRelu.shape[1]), 1, 0, 10)  # flatten
+	# print(outputFlatten.shape)
+	outputFlatten2, flattenFilters2, flattenBias2 = convolution(outputRelu, (outputRelu.shape[0], outputRelu.shape[1]), 1, 0, 2)  # flatten
+	# print(outputFlatten2)
+	# outputFC, weightsFC, biasFC = fullyConnected(outputFlatten, 2)
+	# print("outputFC:", outputFC)
+
+	softmaxResult, softmaxError = softMax(outputFlatten2[0][0], [0, 1])
+	print('result:', softmaxResult,  'error:', softmaxError)
 
 exampleTestData()
 
@@ -173,6 +259,11 @@ trainData, testData = getData("notReceiptData.npy")
 
 # more test data (this uses a real image)
 newImage = Image.open(trainData[0][0])
+newImageExpected = trainData[0][1]
+if newImageExpected == 'receipt':
+	newImageExpected = [1, 0]
+else:
+	newImageExpected = [0, 1]
 # newImage = Image.open("/Users/jack/Documents/programming/notReceipt/dataset/test/0_1.png")
 newImage = np.array(newImage)  # pass this into convolution as image
 newImage = np.reshape(newImage, (newImage.shape[0], newImage.shape[1], 1))
@@ -186,3 +277,15 @@ outputDataPooling = maxPooling(outputData, (2, 2), 2)
 outputReLu = relu(outputDataPooling)
 # print(outputReLu.shape)
 # print(outputReLu)
+
+outputFlatten, flattenFilters, flattenBias = convolution(outputReLu, (outputReLu.shape[0], outputReLu.shape[1]), 1, 0, 10)  # flatten
+# print(outputFlatten)
+# print(outputFlatten.shape)
+outputFlatten2, flattenFilters2, flattenBias2 = convolution(outputReLu, (outputReLu.shape[0], outputReLu.shape[1]), 1, 0, 2)  # flatten
+#print(outputFlatten2)
+
+# outputFC, weightsFC, biasFC = fullyConnected(outputFlatten, 2)
+# print("outputFC:", outputFC)
+
+softmaxResult, softmaxError = softMax(outputFlatten2[0][0], newImageExpected)
+print('result:', softmaxResult,  'error:', softmaxError)
